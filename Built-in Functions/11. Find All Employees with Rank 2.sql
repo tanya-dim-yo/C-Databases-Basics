ac2c9d509c67ddb7bc6f271f7ebd5a7b84b079e0
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2024-01-28 04:34:32 +0200

    Add files via upload

---
+++ Built-in Functions/11. Find All Employees with Rank 2.sql
@@ -0,0 +1,11 @@
+WITH RankedEmployees AS (
+	SELECT EmployeeID, FirstName, LastName, Salary,
+		DENSE_RANK() OVER (PARTITION BY Salary ORDER BY EmployeeID) AS [Rank]
+	FROM Employees 
+	WHERE Salary BETWEEN 10000 AND 50000
+)
+
+SELECT EmployeeID, FirstName, LastName, Salary, [Rank]
+FROM RankedEmployees
+WHERE [Rank] = 2
+ORDER BY Salary DESC;
