8df6c09ea2a5ae17e96b9f4934c684714398c3f9
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2024-02-02 19:21:24 +0200

    Add files via upload

---
+++ Subqueries and Joins/02. Addresses with Towns.sql
@@ -0,0 +1,10 @@
+SELECT TOP 50
+	 [e].[FirstName]
+	,[e].[LastName]
+	,[t].[Name] [Town]
+	,[a].[AddressText]
+FROM
+	[Employees] [e]
+	JOIN [Addresses] [a] ON [e].[AddressID] = [a].[AddressID]
+	JOIN [Towns] [t] ON [a].[TownID] = [t].[TownID]
+ORDER BY [e].[FirstName], [e].[LastName]
